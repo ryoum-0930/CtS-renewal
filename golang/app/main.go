371f9c98c1445539fd338627cdb3b2839778ab20
Kurichi <yuuyapp080@gmail.com>
2021-06-24 21:37:02 +0900

    add data

--- golang/app/main.go
+++ golang/app/main.go
@@ -1,21 +1,61 @@
 package main
 
 import (
-	"net/http"
+	"fmt"
+	"time"
 
-	"github.com/labstack/echo"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
 )
 
-func router(e *echo.Echo) {
-	e.GET("/", helloWorld)
-}
-
-func helloWorld(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello World!")
+type tsts struct {
+	ID   int
+	Name string
+	Abc  string
 }
 
 func main() {
-	e := echo.New()
-	router(e)
-	e.Logger.Fatal(e.Start(":8080"))
+	db := sqlConnect()
+	db.AutoMigrate(&tsts{})
+	defer db.Close()
+
+	db.Create(&tsts{
+		ID:   4,
+		Name: "ddd",
+		Abc:  "aiu",
+	})
+}
+
+func sqlConnect() (database *gorm.DB) {
+	DBMS := "mysql"
+	USER := "root"
+	PASS := "cts-renewal-db-password-BANANA"
+	PROTOCOL := "tcp(mysql-db:3306)"
+	DBNAME := "Test"
+
+	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?charset=utf8mb4&parseTime=true&loc=Asia%2FTokyo"
+
+	count := 0
+	db, err := gorm.Open(DBMS, CONNECT)
+	if err != nil {
+		for {
+			if err == nil {
+				fmt.Println("")
+				break
+			}
+			fmt.Print(".")
+			time.Sleep(time.Second)
+			count++
+			if count > 180 {
+				fmt.Println("")
+				fmt.Println("DB接続失敗")
+				panic(err)
+			}
+			db, err = gorm.Open(DBMS, CONNECT)
+		}
+	}
+
+	fmt.Println("DB接続成功")
+
+	return db
 }
