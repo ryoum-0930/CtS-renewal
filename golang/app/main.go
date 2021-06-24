e19efda4e884b7e1e336276a0c1db5c5a9471f77
Kurichi <yuuyapp080@gmail.com>
2021-06-24 22:07:17 +0900

    go env

--- golang/app/main.go
+++ golang/app/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
+	"github.com/joho/godotenv"
 )
 
 type tsts struct {
@@ -27,13 +30,18 @@ func main() {
 }
 
 func sqlConnect() (database *gorm.DB) {
-	DBMS := "mysql"
-	USER := "root"
-	PASS := "cts-renewal-db-password-BANANA"
-	PROTOCOL := "tcp(mysql-db:3306)"
-	DBNAME := "Test"
+	err := godotenv.Load(fmt.Sprintf("%s.env", os.Getenv("GO_ENV")))
+	if err != nil {
+		log.Fatal(err)
+	}
+	DBMS := os.Getenv("DBMS")
+	USER := os.Getenv("DB_USER")
+	PASS := os.Getenv("DB_PASS")
+	HOST := os.Getenv("DB_HOST")
+	PORT := os.Getenv("DB_PORT")
+	DBNAME := os.Getenv("DB_NAME")
 
-	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?charset=utf8mb4&parseTime=true&loc=Asia%2FTokyo"
+	CONNECT := USER + ":" + PASS + "@tcp(" + HOST + ":" + PORT + ")/" + DBNAME + "?charset=utf8mb4&parseTime=true&loc=Asia%2FTokyo"
 
 	count := 0
 	db, err := gorm.Open(DBMS, CONNECT)
